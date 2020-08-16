1fe23ed47172c98aa7b55fb28f53e58c1809df31
LeeonardoVargas <leu1607@hotmail.com>
2020-08-16 11:13:16 -0300

    Adicionando JSON com as informações e configurando rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { Router, Request, Response } from 'express';
 
+import data from './data.json';
+
 const routes = Router();
 
-routes.get('/', async (request: Request, response: Response): Promise<Response> => {
-    return response.json({ok: true});
+routes.get('/planets', async (request: Request, response: Response): Promise<Response> => {
+    return response.json(data);
+  },
+);
+
+routes.get('/planet/:id', async (request: Request, response: Response): Promise<Response> => {
+    const { id } = request.params;
+    const planet = data.planets.filter( planet => planet.id === id )[0];
+
+    return response.json(planet);
   },
 );
 
